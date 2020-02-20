fb812ea115ad8a87a0cb6a177d96eab83402714f
ASUTOSH GHANTO <44160439+asutoshgha@users.noreply.github.com>
2020-02-20 17:15:17 +0530

    Create heapmax.cpp

---
+++ heapmax.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	vector<int> heap={6,4,7,9,5,3,10};
+	make_heap(heap.begin(),heap.end(),greater<int>);
+	for (auto i = heap.begin(); i != heap.end(); ++i)
+        	cout << *i <<endl;
+       	heap.push_back(50);
+	push_heap(heap.begin(),heap.end());
+	cout<<endl;
+	for (auto i = heap.begin(); i != heap.end(); ++i)
+        	cout << *i <<endl;	
+	pop_heap(heap.begin(),heap.end());
+	cout<<endl;
+	for (auto i = heap.begin(); i != heap.end(); ++i)
+        	cout << *i <<endl;
+	heap.pop_back();
+	for (auto i = heap.begin(); i != heap.end(); ++i)
+        	cout << *i <<endl;
+	
+ 		
+}
