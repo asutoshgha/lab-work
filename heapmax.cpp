aa724c8df0bfbc2a1d7aaf37eca4b140ddeff1b8
ASUTOSH GHANTO <44160439+asutoshgha@users.noreply.github.com>
2020-02-20 17:16:38 +0530

    Update heapmax.cpp

--- heapmax.cpp
+++ heapmax.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	vector<int> heap={6,4,7,9,5,3,10};
-	make_heap(heap.begin(),heap.end(),greater<int>);
+	make_heap(heap.begin(),heap.end());
 	for (auto i = heap.begin(); i != heap.end(); ++i)
         	cout << *i <<endl;
        	heap.push_back(50);
